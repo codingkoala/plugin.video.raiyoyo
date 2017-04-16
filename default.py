5efc8812f0bb211cc85628fd18db01d2a2adb3c9
wizy <wizy@koala.local>
2017-04-16 16:15:21 +0200

    v1.0.1

--- default.py
+++ default.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-import xbmcaddon
-import xbmc
-import xbmcplugin
-import xbmcgui
-import os
-import sys
-import re 
 from resources.lib.raiyoyo import Raiyoyo
 from phate89lib import kodiutils, staticutils
 
@@ -15,33 +8,29 @@ raiyoyo = Raiyoyo()
 raiyoyo.log = kodiutils.log
 
 def stamp_ep(ep):
-    ep['mediatype'] = 'video'
-    xbmc.log("STAMP_EP:")
-    xbmc.log(str(ep))
-    kodiutils.addListItem("[COLOR blue]"+ep["title"]+"[/COLOR]", {'mode':'playRaiyoyo', 'url': ep["url"]}, thumb=ep["thumbs"],videoInfo=ep,isFolder=False)
+  ep['mediatype'] = 'video'
+  xbmc.log("STAMP_EP:")
+  xbmc.log(str(ep))
+  kodiutils.addListItem("[COLOR blue]"+ep["title"]+"[/COLOR]", {'mode':'playRaiyoyo', 'url': ep["url"]}, thumb = ep["thumbs"], videoInfo = ep, isFolder = False)
 
 def root():
-    kodiutils.addListItem("Programmi",{'mode':'elenco_video'})
-    kodiutils.endScript()
+  kodiutils.addListItem("Programmi",{'mode':'elenco_video'})
+  kodiutils.endScript()
 
 def elenco_video_groupList():
-    for group in raiyoyo.get_url_groupList():
-      kodiutils.addListItem(group["title"], {'mode':'elenco_puntate', 'id': group["id"]})
-    kodiutils.endScript()
+  for group in raiyoyo.get_url_groupList():
+    kodiutils.addListItem(group["title"], {'mode':'elenco_puntate', 'id': group["id"]})
+  kodiutils.endScript()
 
 def elenco_puntate(id):
-    kodiutils.setContent('videos')
-    for pun in raiyoyo.get_url_punList(id):
-      stamp_ep(pun)
-    kodiutils.endScript()
-
+  kodiutils.setContent('videos')
+  for pun in raiyoyo.get_url_punList(id):
+    stamp_ep(pun)
+  kodiutils.endScript()
 
 def playRaiyoyo(streamUrl):
-    kodiutils.setResolvedUrl(streamUrl)
-    kodiutils.setResolvedUrl(solved=False)
-    xbmc.executebuiltin('XBMC.PlayerControl(Play)')
+  kodiutils.setResolvedUrl(streamUrl)
   
-
 # parameter values
 params = staticutils.getParams()
 
@@ -53,7 +42,6 @@ if 'mode' in params:
       elenco_puntate(params['id'])
   elif params['mode'] == "playRaiyoyo":
     playRaiyoyo(params['url'])
-
 else:
   root()
 
